cadd1539cbc730f9c20d5356d830cc09cf54bfc8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-27 14:35:19 +0200

    Update FriendshipService.php

--- app/Services/FriendshipService.php
+++ app/Services/FriendshipService.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace App\Services;
+
+use App\User;
+
+class FriendShipService
+{
+  User $user;
+  User $friend;
+  public  __construct(User $user){
+    $this->user=$user;
+  }
+  public function friend(User $friend){
+    $this->friend=$friend
+  }
+  public function addFirend(){
+      
+  }
+  public function block(){
+      
+  }
+  public function removeFriend(){
+      
+  }
+    
+}
