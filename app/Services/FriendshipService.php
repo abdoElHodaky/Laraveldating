68048d151a570ba7b4f4d4811069c312d75de5aa
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-27 14:44:03 +0200

    Update FriendshipService.php

--- app/Services/FriendshipService.php
+++ app/Services/FriendshipService.php
@@ -7,21 +7,28 @@ use App\User;
 class FriendShipService
 {
   User $user;
-  User $friend;
   public  __construct(User $user){
     $this->user=$user;
   }
-  public function friend(User $friend){
-    $this->friend=$friend
+ /* public function friend(User $friend){
+    $this->friend=$friend;
+    return $friend;
+  }*/
+  public function friendRequest (User $friend){
+     // $user->acceptFriendRequest($sender);
+    $this->user->befriend($friend);
   }
-  public function addFirend(){
-      
+  public function block(User $friend){
+      $this->user->unblockFriend($friend);
   }
-  public function block(){
-      
+  public function removeFriend(User $friend){
+      $this->user->unfriend($friend);
   }
-  public function removeFriend(){
-      
+  public function confirmFriend(User $sender){
+      $user->acceptFriendRequest($sender);
+  }
+  public function denyFriend(User $sender){
+      $user->denyFriendRequest($sender);
   }
     
 }
