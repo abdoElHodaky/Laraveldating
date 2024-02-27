69178b976f1f0b66a3dfa724b5292d042b43ca7e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-27 14:59:17 +0200

    Update FriendshipService.php

--- app/Services/FriendshipService.php
+++ app/Services/FriendshipService.php
@@ -18,17 +18,33 @@ class FriendShipService
      // $user->acceptFriendRequest($sender);
     $this->user->befriend($friend);
   }
-  public function block(User $friend){
-      $this->user->unblockFriend($friend);
-  }
   public function removeFriend(User $friend){
       $this->user->unfriend($friend);
   }
-  public function confirmFriend(User $sender){
+  private function confirmFriend(User $sender){
       $user->acceptFriendRequest($sender);
   }
-  public function denyFriend(User $sender){
+  private function denyFriend(User $sender){
       $user->denyFriendRequest($sender);
   }
+  public function confirmOrdenyFriend(User $sender,$confirm=1){
+      if ($confirm==1){
+          confirmFriend($sender);
+      }
+      else{
+          denyFriend($sender);
+      }
+  }
+public function blockOrUnblock(User $friend,$block=0){
+    if($block==1){
+        $this->user->blockFriend($friend);
+    }
+    else{
+        $this->user->unblockFriend($friend);
+    }
+}
+public function getFriends(){
+   return $this->user->getAllFriendships();
+}
     
 }
